2cd738efe1de9f35b6ecc18ac2b885fda054eaf8
Frimas9119 <90787948+Frimas9119@users.noreply.github.com>
2021-10-06 16:05:34 +0300

    Add files via upload

---
+++ lab3.4/lab3.4/lab3.4.cpp
@@ -0,0 +1,24 @@
+﻿// Lab_03_4.cpp
+// < Тимчук Остап >
+// Лабораторна робота No 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 8
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double r; //promihne
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "r = "; cin >> r;
+	// розгалуження в повній формі
+	if ((y <= sqrt(pow(r,2)-pow(x,2)) && x >= 0) ||
+		(y >= abs(x) && y <= -r && y <= r))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
